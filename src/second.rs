b48fbc510fe220df29875a1400ded64185bb96d8
sasakiy84 <sasakiy84@g.ecc.u-tokyo.ac.jp>
2024-05-06 23:56:56 +0900

    module 2 wip

--- src/second.rs
+++ src/second.rs
@@ -51,6 +51,11 @@ impl<T> List<T> {
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
     }
+
+    #[allow(clippy::should_implement_trait)]
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -60,6 +65,16 @@ impl<T> Drop for List<T> {
         //     cur_link = boxed_node.next.take();
         // }
         while self.pop_node().is_some() {}
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // IntoIter は tuple 型の struct なので、 0 でアクセスする
+        self.0.pop()
     }
 }
 
@@ -113,5 +128,20 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.peek_mut(), Some(&mut 42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
     }
 }
